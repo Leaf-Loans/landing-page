75fb25717ec5123ae9f2b6bd6e4d1c7cf6bf8a02
Prasenjit Datta <prasenjit2311@gmail.com>
2026-04-20 15:11:56 +0530

    feat: add full landing page with all sections and logo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,8 @@
-// interactions
+const toggle = document.querySelector('.nav-toggle');
+const links = document.querySelector('.nav-links');
+
+if (toggle && links) {
+  toggle.addEventListener('click', () => {
+    links.classList.toggle('open');
+  });
+}
